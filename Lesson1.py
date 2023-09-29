5c7d94dfdd9fb03ed91a99e00f5749303e2bb78f
theegreate <122451374+theegreate@users.noreply.github.com>
2023-09-29 14:00:52 +0300

    Add files via upload

---
+++ Lesson1.py
@@ -0,0 +1,10 @@
+# EUNICE NYABOKE ONKUNDI
+#SCT211-0544/2022
+
+def sum(a, b):
+    return (a + b)
+
+a = int(input('Enter 1st number: '))
+b = int(input('Enter 2nd number: '))
+
+print(f'Sum of {a} and {b} is {sum(a, b)}')
